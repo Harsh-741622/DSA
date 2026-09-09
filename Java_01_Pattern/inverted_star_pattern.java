132375f2efeefa108f8f26cda3d6dd244b7eef7f
Harsh Sharma <harshsharma.7416.in@gmail.com>
2026-09-09 23:11:49 +0530

    Re-structuring files

--- Java_01_Pattern/inverted_star_pattern.java
+++ Java_01_Pattern/inverted_star_pattern.java
@@ -1,12 +1,13 @@
+package Java_01_Pattern;
 
 import java.util.*;
-public class star_pattern {
+public class inverted_star_pattern {
         public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n : ");
         int n = sc.nextInt();
 
-        for(int i=1 ; i<=n ; i++){
+        for(int i=n ; i>0 ; i--){
             for(int j=1 ; j<=i ; j++) {
                 System.out.print("*");
             }
